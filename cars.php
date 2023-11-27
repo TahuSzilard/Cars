22c570636afc210fb8a1adbb433ece32fc4a06ec
3041TAN-15 <3041TAN-15@boronkay.local>
2023-11-27 10:11:33 +0100

    commit

--- cars.php
+++ cars.php
@@ -5,10 +5,47 @@ ini_set('memorí_limit', '560M');
 $fileName = 'car-db.csv';
 $csvData = getCsvData($fileName);
 
-$arr = array('first' => 'a','second'=>'b',);
-$key = array_search('a', $arr);
+if(empty($csvData))
+{
+    echo"Nem található adat a csv fájlban.";
+    return false;
+}
 
 $header = $csvData[0];
+
 $keyMaker = array_search('make', $header);
+$result = [];
+$maker = '';
+$model = '';
+$makers = [];
+$isHeader = true;
+foreach($csvData as $data)
+{
+    if(!is_array($data))
+    {
+        continue;
+    }
+
+    if($isHeader)
+    {
+        $isHeader = false;
+        continue;
+    }
+
+    if($maker != $data[$keyMaker])
+        {
+            $maker = $data[$keyMaker];
+            $makers [] = $maker;
+        }
+
+    if($model != $data[$keyMaker])
+        {
+            $model = $data[$keyMaker];
+            $result[$maker][] = $model;
+        }
+}
+print_r($result);
+print_r($makers);
+
 
 ?>
--- csv-tools.php
+++ csv-tools.php
@@ -1,26 +1,20 @@
 <?php
-function getCsvData($fileName, $withHeader = true)
+function getCsvData($fileName)
 {
     if(!file_exists($fileName)){
         echo "$filename nem található";
         return false;
     }
     $csvFile = fopen($fileName, 'r');
-    $header = fgetcsv($csvFile);
-    if ($withHeader) 
-    {
-        $Lines = $header;
-    }
-    else
-    {
-        $lines = [];
-    }
-    while (!feof($csvFile)){
+    $lines = [];
+    while (! feof($csvFile)){
         $line = fgetcsv($csvFile);
         $lines[] = $line;
     }
     fclose($csvFile);
+
     return $lines;
- 
+    
+   
 }
 ?>
